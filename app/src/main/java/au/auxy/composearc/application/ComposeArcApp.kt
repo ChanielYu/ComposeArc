efa3eafc0ab75f0e465df1a3ce283e678626cd19
Chaniel Yu <chaniel.yu@gmail.com>
2023-05-27 18:52:32 +1000

    Init commit

---
+++ app/src/main/java/au/auxy/composearc/application/ComposeArcApp.kt
@@ -0,0 +1,7 @@
+package au.auxy.composearc.application
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeArcApp : MultiDexApplication()
---
+++ app/src/main/java/au/auxy/composearc/playground/ui/fragment/PlaygroundFragment.kt
@@ -0,0 +1,8 @@
+package au.auxy.composearc.playground.ui.fragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PlaygroundFragment:Fragment() {
+}
