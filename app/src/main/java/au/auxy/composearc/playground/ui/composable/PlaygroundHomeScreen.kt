fa887277ff39b9535bea8bd3096d7ee7b8f3d6ec
Chaniel Yu <chaniel.yu@gmail.com>
2023-05-28 21:07:21 +1000

    Compose logging

--- app/src/main/java/au/auxy/composearc/playground/ui/composable/PlaygroundHomeScreen.kt
+++ app/src/main/java/au/auxy/composearc/playground/ui/composable/PlaygroundHomeScreen.kt
@@ -1,17 +1,23 @@
 package au.auxy.composearc.playground.ui.composable
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import au.auxy.composearc.application.AppConst.COMPOSE_TAG
 import au.auxy.composearc.ui.theme.ComposeArcTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PlaygroundHomeScreen() {
+fun PlaygroundHomeScreen(navigateDetail: () -> Unit) {
+    Log.d(COMPOSE_TAG, "PlaygroundHomeScreen")
     Surface(
+        onClick = navigateDetail,
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background,
         contentColor = MaterialTheme.colorScheme.tertiary
@@ -22,6 +28,7 @@ fun PlaygroundHomeScreen() {
 
 @Composable
 private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Log.d(COMPOSE_TAG, "PlaygroundHomeScreen->Greeting")
     Text(
         text = "Hello $name!",
         modifier = modifier
--- app/src/main/java/au/auxy/composearc/playground/ui/fragment/PlaygroundFragment.kt
+++ app/src/main/java/au/auxy/composearc/playground/ui/fragment/PlaygroundFragment.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import au.auxy.composearc.playground.ui.composable.PlaygroundDetailScreen
 import au.auxy.composearc.playground.ui.composable.PlaygroundHomeScreen
 import au.auxy.composearc.ui.fragment.BaseComposeFragment
 import au.auxy.composearc.ui.theme.ComposeArcTheme
@@ -15,9 +16,16 @@ class PlaygroundFragment : BaseComposeFragment() {
     override fun HostScreenContent() = ComposeArcTheme {
         val navController = rememberNavController()
         NavHost(navController = navController, startDestination = "PlaygroundHome") {
-            composable("PlaygroundHome") {
-                PlaygroundHomeScreen()
+            composable(route = "PlaygroundHome") {
+                PlaygroundHomeScreen { navController.navigate("PlaygroundDetail") }
+            }
+            composable(route = "PlaygroundDetail") {
+                PlaygroundDetailScreen { navigateBack() }
             }
         }
+    }
+
+    private fun navigateBack() {
+        activity?.onBackPressedDispatcher?.onBackPressed()
     }
 }
